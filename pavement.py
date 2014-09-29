6818cf2d03d78dcc150c477608ee0105ca7eb4b7
Christian Fobel <christian@fobel.net>
2014-09-29 07:52:23 -0400

    Add paver task to generate `DeviceVector` types
     - Add `paver` task to generate a `DeviceVector` definition for each of
       the following types:
      - `int8_t`
      - `uint8_t`
      - `int16_t`
      - `uint16_t`
      - `int32_t`
      - `uint32_t`
      - `int64_t`
      - `uint64_t`
      - `float`
      - `double`

--- pavement.py
+++ pavement.py
@@ -1,4 +1,4 @@
-from paver.easy import task, needs
+from paver.easy import task, needs, path
 from paver.setuputils import setup, install_distutils_tasks
 
 import version
@@ -16,7 +16,45 @@ setup(name='cythrust',
 
 
 @task
-@needs('generate_setup', 'minilib', 'setuptools.command.sdist')
+def generate_device_vector_source():
+    '''
+    Generate Cython code from template for `DeviceVector` extension types.
+    '''
+    import jinja2
+
+    package_root = path(__file__).abspath().parent
+    for ctype, dtype in (('int8_t', 'np.int8'),
+                         ('uint8_t', 'np.uint8'),
+                         ('int16_t', 'np.int16'),
+                         ('uint16_t', 'np.uint16'),
+                         ('int32_t', 'np.int32'),
+                         ('uint32_t', 'np.uint32'),
+                         ('int64_t', 'np.int64'),
+                         ('uint64_t', 'np.uint64'),
+                         ('float', 'np.float32'),
+                         ('double', 'np.float64')):
+        dtype_module = package_root.joinpath('cythrust', 'device_vector',
+                                             dtype[3:])
+        dtype_module.makedirs_p()
+
+        with dtype_module.joinpath('device_vector.pyx').open('wb') as output:
+            template_path = package_root.joinpath('cythrust', 'device_vector',
+                                                  'device_vector.pyxt')
+            template = jinja2.Template(template_path.bytes())
+            output.write(template.render({'C_DTYPE': ctype,
+                                          'NP_DTYPE': dtype}))
+
+        with dtype_module.joinpath('__init__.py').open('wb') as output:
+            template_path = package_root.joinpath('cythrust', 'device_vector',
+                                                  '__init__.pyt')
+            template = jinja2.Template(template_path.bytes())
+            output.write(template.render({'C_DTYPE': ctype,
+                                          'NP_DTYPE': dtype}))
+
+
+@task
+@needs('generate_device_vector_source', 'generate_setup', 'minilib',
+       'setuptools.command.sdist')
 def sdist():
     """Overrides sdist to make sure that our setup.py is generated."""
     pass
