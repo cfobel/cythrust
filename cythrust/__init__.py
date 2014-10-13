b2056cd23b3af9ee3e5fb8d2526ccf85befec46a
Christian Fobel <christian@fobel.net>
2014-10-13 11:16:23 -0400

    Add DeviceVectorCollection...
    Add `DeviceVectorCollection` to store collections of `DeviceVector`
    instances, where a `DeviceVectorView` is also created for each
    `DeviceVector`, enabling easy slicing of vectors for Thrust functions.
    
    When possible, it is preferable to use the `DeviceDataFrame`, but the
    `DeviceDataFrame` requires that all input arrays are of the same length.
    The `DeviceVectorCollection` may contain arrays of varying length.

--- cythrust/__init__.py
+++ cythrust/__init__.py
@@ -6,7 +6,84 @@ import pandas as pd
 import cythrust.device_vector as dv
 
 
-class DeviceDataFrame(object):
+class DeviceVectorCollection(object):
+    '''
+    A container of `cythrust.device_vector.DeviceVector` instances.
+    Can be initialized by:
+        - A dictionary-like container of `numpy.ndarray` instances.
+        - A `pandas.DataFrame`.
+
+    The types of the device vector instances will be inferred based on the
+    types of the input arrays.
+
+
+    Known issues
+    ------------
+
+     - Only numeric types are accepted.
+    '''
+    def __init__(self, data=None):
+        if isinstance(data, dict):
+            self._data_dict = OrderedDict([(k, dv.from_array(v))
+                                           for k, v in data.iteritems()])
+        elif isinstance(data, pd.DataFrame):
+            self._data_dict = OrderedDict([(c, dv.from_array(data[c]
+                                                             .values))
+                                           for c in data.columns])
+        elif data is None:
+            self._data_dict = OrderedDict()
+        elif data is not None:
+            raise ValueError('Unsupported input data type.')
+
+        self._view_dict = OrderedDict([(k, dv.view_from_vector(v))
+                                       for k, v in self._data_dict
+                                       .iteritems()])
+
+    def add(self, column_name, column_data, dtype=None):
+        if dtype is None:
+            column_data = column_data.astype(dtype)
+        self._data_dict[column_name] = dv.from_array(column_data)
+        self._view_dict[column_name] = dv.view_from_vector(
+            self._data_dict[column_name], first_i=start, last_i=end)
+
+    def drop(self, column_name):
+        del self._view_dict[column_name]
+        del self._data_dict[column_name]
+
+    def base(self):
+        result = DeviceVectorCollection()
+        result._data_dict = self._data_dict
+        result._view_dict = OrderedDict([(k, dv.view_from_vector(v))
+                                         for k, v in result._data_dict
+                                         .iteritems()])
+        return result
+
+    def as_arrays(self):
+        return OrderedDict([(k, v[:]) for k, v in self._view_dict.iteritems()])
+
+    @property
+    def d(self):
+        return self._data_dict
+
+    @property
+    def v(self):
+        return self._view_dict
+
+    def __getitem__(self, key):
+        # TODO: Return view?
+        return self._view_dict[key][:]
+
+    @property
+    def columns(self):
+        return self._view_dict.keys()
+
+    @property
+    def size(self):
+        return OrderedDict([(k, v.size)
+                            for k, v in self._view_dict.iteritems()])
+
+
+class DeviceDataFrame(DeviceVectorCollection):
     '''
     A container of `cythrust.device_vector.DeviceVector` instances.
     Can be initialized by:
@@ -25,55 +102,33 @@ class DeviceDataFrame(object):
     def __init__(self, data=None):
         if isinstance(data, dict):
             assert(len(set([v.size for v in data.itervalues()])) == 1)
-            self._data_dict = OrderedDict([(k, dv.from_array(v))
-                                           for k, v in data.iteritems()])
-        elif isinstance(data, pd.DataFrame):
-            self._data_dict = OrderedDict([(c, dv.from_array(data[c]
-                                                             .values))
-                                           for c in data.columns])
-        elif data is None:
-            self._data_dict = OrderedDict()
-        elif data is not None:
-            raise ValueError('Unsupported input data type.')
-
-        self._view_dict = OrderedDict([(k, dv.view_from_vector(v))
-                                       for k, v in self._data_dict
-                                       .iteritems()])
+        super(DeviceDataFrame, self).__init__(data)
 
     def add(self, column_name, column_data=None, dtype=None):
         if dtype is None and column_data is None:
             raise ValueError('At least one of `column_data` or `dtype` must '
                              'be provided.')
-        size = self._data_dict.values()[0].size
+        if len(self._data_dict):
+            size = self._data_dict.values()[0].size
+        else:
+            size = None
         if column_data is None:
+            if size is None:
+                raise ValueError('Size cannot be inferred when the data frame '
+                                 'does not have any existing columns.')
             column_data = np.zeros(size, dtype=dtype)
-        elif dtype is None:
+        elif dtype is not None:
             column_data = column_data.astype(dtype)
         self._data_dict[column_name] = dv.from_array(column_data)
-        sample_view = self._view_dict.values()[0]
+        if len(self._view_dict):
+            sample_view = self._view_dict.values()[0]
+            start = sample_view.first_i
+            end = sample_view.last_i
+        else:
+            start = 0
+            end = self._data_dict.values()[0].size - 1
         self._view_dict[column_name] = dv.view_from_vector(
-            self._data_dict[column_name], first_i=sample_view.first_i,
-            last_i=sample_view.last_i)
-
-    def drop(self, column_name):
-        del self._view_dict[column_name]
-        del self._data_dict[column_name]
-
-    def base(self):
-        result = DeviceDataFrame()
-        result._data_dict = self._data_dict
-        result._view_dict = OrderedDict([(k, dv.view_from_vector(v))
-                                         for k, v in result._data_dict
-                                         .iteritems()])
-        return result
-
-    @property
-    def d(self):
-        return self._data_dict
-
-    @property
-    def v(self):
-        return self._view_dict
+            self._data_dict[column_name], first_i=start, last_i=end)
 
     def as_dataframe(self):
         return pd.DataFrame(OrderedDict([(k, v[:])
@@ -136,7 +191,6 @@ class DeviceDataFrame(object):
             start += sample_view.first_i
             end += sample_view.first_i
             end = min(sample_view.last_i + 1, end)
-        print start, end
         view._view_dict = OrderedDict([(k, dv.view_from_vector(v, start,
                                                                end - 1))
                                         for k, v in
